5e219374dae19d244dc065938ec14342b8fa651c
Tomas <dev.tomas.ivan@gmail.com>
2018-07-24 11:07:18 +0200

    created SAMLClient

---
+++ SamlClient/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using SamlClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SamlClient.Controllers
+{
+    [Authorize]
+    public class AccountController: Controller
+    {
+        [AllowAnonymous]
+        public ActionResult SignIn()
+        {
+            RBTeam.Saml2Lib.Saml2Module.Instance.SignIn();
+            return new EmptyResult();
+        }
+
+        [AllowAnonymous]
+        public ActionResult SignInByHash(string hash)
+        {
+            RBTeam.Saml2Lib.Saml2Module.Instance.SignIn();
+            return new EmptyResult();
+        }
+
+        [Authorize]
+        public ActionResult AccountInfo()
+        {
+            return View(new AccountModel());
+        }
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            RBTeam.Saml2Lib.Saml2Module.Instance.SignOut("~/");
+            return new EmptyResult();
+        }
+    }
+}
---
+++ SamlClient/Models/AccountModel.cs
@@ -0,0 +1,95 @@
+﻿using RBTeam.Saml2Lib.Data.Infra;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Web;
+
+namespace SamlClient.Models
+{
+    public class AccountModel
+    {
+        private readonly HttpContext context;
+
+        [Display(Name = "Meno pouzivatela")]
+        public string Name
+        {
+            get
+            {
+                return GetSamlPrincipal()?.Identity.Name;
+            }
+        }
+
+        [Display(Name = "Typ prihlasenia")]
+        public string AuthenticationType
+        {
+            get
+            {
+                return GetSamlPrincipal()?.Identity.AuthenticationType;
+            }
+        }
+
+        [Display(Name = "Subject Name Id")]
+        public string NameID
+        {
+            get
+            {
+                return GetSamlPrincipal()?.NameID;
+            }
+        }
+
+        [Display(Name = "Session index")]
+        public string SessionIndex
+        {
+            get
+            {
+                return GetSamlPrincipal()?.SessionIndex;
+            }
+        }
+
+        [Display(Name = "Claims")]
+        public IEnumerable<Tuple<string, string>> Claims
+        {
+            get
+            {
+                List<Tuple<string, string>> list = new List<Tuple<string, string>>();
+                ISaml2Principal samlPrincipal = GetSamlPrincipal();
+                if (samlPrincipal?.Claims == null)
+                {
+                    return list;
+                }
+                foreach (string key in samlPrincipal.Claims.Keys)
+                {
+                    list.AddRange(from value in samlPrincipal.Claims[key]
+                                  select new Tuple<string, string>(key, value));
+                }
+                return list;
+            }
+        }
+
+        [Display(Name = "Roles")]
+        public IEnumerable<string> Roles
+        {
+            get
+            {
+                return GetSamlPrincipal()?.Roles;
+            }
+        }
+
+        public SecurityToken BootstrapToken => (GetSamlPrincipal().Identity as ISaml2Identity)?.BootstrapToken;
+
+        public AccountModel()
+        {
+            if (HttpContext.Current.User.Identity.IsAuthenticated)
+            {
+                context = HttpContext.Current;
+            }
+        }
+
+        private ISaml2Principal GetSamlPrincipal()
+        {
+            return (ISaml2Principal)(context?.User);
+        }
+    }
+}
